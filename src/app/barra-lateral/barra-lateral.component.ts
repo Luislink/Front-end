7cbfc00a00c5e5debe22af70e6732321ca060520
Luis Juarez <juarezluisantonio012@gmail.com>
2023-05-09 12:02:17 -0300

    Actualizado el front sin enlace -Registrar-

--- src/app/barra-lateral/barra-lateral.component.ts
+++ src/app/barra-lateral/barra-lateral.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { persona } from '../model/persona.model';
+import { PersonaService } from '../service/persona.service';
 
 @Component({
   selector: 'app-barra-lateral',
   templateUrl: './barra-lateral.component.html',
   styleUrls: ['./barra-lateral.component.css']
 })
-export class BarraLateralComponent {
+export class BarraLateralComponent implements OnInit {
+  persona: persona = new persona("","");
+  constructor(public personaService: PersonaService){
+
+  }
+  ngOnInit(): void {
+    this.personaService.getPersona().subscribe(data =>{this.persona = data})
+  }
 
 }
